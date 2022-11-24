30a8c43a91044ff784f17ec0bb6514999fc4fa11
Meerwaldten <Mikkel_m_j@hotmail.com>
2022-11-24 11:23:22 +0100

    Meerwaldt laver ballade 1

--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -8,11 +8,12 @@ export const NavbarComponent = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <Nav.Link as={Link} to="/bowlingReservation">Bowling Reservation </Nav.Link>
-
+                    <Nav.Link as={Link} to="/airhockeyreservation">Airhockey Reservation </Nav.Link>
+                    <Nav.Link as={Link} to="/diningReservation">Dining Reservation </Nav.Link>
+                    <Nav.Link as={Link} to="/login">Login </Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
