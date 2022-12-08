148e0249f3c8d89ba22c72202e937d1707a68f10
asgerbirk <asger.birk@gmail.com>
2022-12-08 14:21:53 +0100

    first

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,14 @@ export const Home = () =>{
 
     return(
         <div className="home">
+            <div className="overlay"></div>
             <video src={zandobowl} autoPlay loop muted />
+            <div className="welcome">
+                <h1>Velkommen til XPBowl</h1>
+                <h4>Klik her for at booke en tid</h4>
+
+            </div>
+
         </div>
 
 
